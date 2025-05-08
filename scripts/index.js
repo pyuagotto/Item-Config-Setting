f47d1b2f8a3a8874dcd6b4de6ba2479dffdc8c49
pyuagotto <pyuagotto@gmail.com>
2025-05-08 16:21:45 +0900

    1.21.80に対応 カスタムコマンドを使用するように変更

--- scripts/index.js
+++ scripts/index.js
@@ -1,67 +1,72 @@
 //@ts-check
-import { world, system, Player } from '@minecraft/server';
-import { ModalFormData, FormCancelationReason } from '@minecraft/server-ui';
-import config from './config.js';
+import { system, Player, CommandPermissionLevel, CustomCommandOrigin, CustomCommandStatus } from '@minecraft/server';
+import { ModalFormData } from '@minecraft/server-ui';
+
+system.beforeEvents.startup.subscribe((ev) => {
+    const registerCommand = function(name, description, callback) {
+        ev.customCommandRegistry.registerCommand(
+            {
+                name,
+                description,
+                permissionLevel: CommandPermissionLevel.Admin,
+            },
+            callback
+        );
+    };
+
+    registerCommand(
+        "pyuagotto:itemconfig",
+        "アイテムの名前と説明を変更します",
+        openConfigForm
+    );
+});
 
 /**
  * 
- * @param {Player} source 
- * @param {Boolean} sendmsg 
+ * @param {CustomCommandOrigin} origin 
+ * @returns {{ status: CustomCommandStatus, message?: string} | undefined }
  */
-const openConfigForm = function(source, sendmsg){
-    const container = source.getComponent("minecraft:inventory")?.container;
-    const slot = source.selectedSlotIndex;
-    const mainhandItem = container?.getItem(slot);
-   
-    if(mainhandItem){
-        if(sendmsg) source.sendMessage("§bチャットを閉じるとformが表示されます§r");
-        const nowName = mainhandItem.nameTag || "";
-        const nowLore = mainhandItem.getLore() || [];
+const openConfigForm = function(origin){
+    if(origin.sourceEntity instanceof Player){
+        const player = origin.sourceEntity;
+        const container = player.getComponent("minecraft:inventory")?.container;
+        const slot = player.selectedSlotIndex;
+        const mainhandItem = container?.getItem(slot);
+       
+        if(mainhandItem){
+            const nowName = mainhandItem.nameTag || "";
+            const nowLore = mainhandItem.getLore() || [];
+    
+            const modalForm = new ModalFormData();
+            modalForm.title("アイテムの設定");
+            modalForm.textField("Name", "名前", { defaultValue: nowName });
+    
+            nowLore.forEach((lore, index) => {
+                modalForm.textField(`Lore${index + 1}`, "説明", { defaultValue: lore });
+            });
+            
+            modalForm.textField(`Lore${nowLore.length + 1}`, "説明", { defaultValue: "" }); // 空のフィールドを追加
 
-        const modalForm = new ModalFormData();
-        modalForm.title("アイテムの設定");
-        modalForm.textField("Name", "名前", nowName);
-
-        nowLore.forEach((lore, index) => {
-            modalForm.textField(`Lore${index + 1}`, "説明", lore);
-        });
+            system.run(()=>{
+                modalForm.show(player).then(modalFormResponse => {
+                    if(!modalFormResponse.canceled && modalFormResponse.formValues){
+                        const [newName, ...loreValues] = modalFormResponse.formValues;
+                        const loreList = loreValues
+                            .filter(lore => typeof lore === "string")
+                            .map(lore => lore.trim())
+                            .filter(lore => lore !== "");
         
-        modalForm.textField(`Lore${nowLore.length + 1}`, "説明", ""); // 空のフィールドを追加
-
-        //@ts-ignore
-        modalForm.show(source).then(modalFormResponse => {
-            if(!modalFormResponse.canceled && modalFormResponse.formValues){
-                const [newName, ...loreValues] = modalFormResponse.formValues;
-                const loreList = loreValues
-                    .filter(lore => typeof lore === "string")
-                    .map(lore => lore.trim())
-                    .filter(lore => lore !== "");
-
-                if(typeof newName === "string") mainhandItem.nameTag = newName;
-                mainhandItem.setLore(loreList);
-
-                container?.setItem(slot, mainhandItem); 
-            }else if(modalFormResponse.cancelationReason == FormCancelationReason.UserBusy){
-                openConfigForm(source, false);
-            }
-        });
-    }else{
-        source.sendMessage("§cこのコマンドを使用するにはアイテムをメインハンドに持つ必要があります§r");
-    }
-};
-
-world.beforeEvents.chatSend.subscribe((ev) => {
-    const { message, sender } = ev;
-
-    if(message == config.commandPrefix + "itemconfig"){
-        ev.cancel = true;
-
-        if(sender.isOp()){
-            system.run(() => {
-                openConfigForm(sender, true);
+                        if(typeof newName === "string") mainhandItem.nameTag = newName;
+                        mainhandItem.setLore(loreList);
+        
+                        container?.setItem(slot, mainhandItem);
+                    }
+                });
             });
         }else{
-            sender.sendMessage("§cこのコマンドを実行する権限がありません\n必要権限:OP§r");
+            return { status: CustomCommandStatus.Failure, message: `このコマンドを使用するにはアイテムをメインハンドに持つ必要があります` };
         }
+    }else{
+        return { status: CustomCommandStatus.Failure, message: `このコマンドはプレイヤー以外に対して実行できません` };
     }
-});
+};
